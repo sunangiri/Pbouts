0ba029d619c2b617c47a1cfe660a3410cae6240f
M Khoirul Risqi <82764602+risqikhoirul@users.noreply.github.com>
2023-11-02 07:16:39 +0700

    Update Rekening.java

--- Rekening.java
+++ Rekening.java
@@ -1,1 +1,25 @@
 
+public class Rekening {
+ private String noRekening;
+ private double saldo;
+ public Rekening(String noRekening, double saldoAwal) {
+ this.noRekening = noRekening;
+ this.saldo = saldoAwal;
+ }
+ public void simpanUang(double jumlah) {
+ saldo += jumlah;
+ }
+ public void ambilUang(double jumlah) {
+ if (saldo >= jumlah) {
+ saldo -= jumlah;
+ } else {
+ System.out.println("Saldo tidak mencukupi untuk penarikan.");
+ }
+ }
+ public String getNoRekening() {
+ return noRekening;
+ }
+ public double getSaldo() {
+ return saldo;
+ }
+}
